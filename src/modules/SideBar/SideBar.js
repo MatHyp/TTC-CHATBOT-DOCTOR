e50446dcfd19b3817727a658b58f58c1fe2c5819
windforce2007 <94519254+windforce2007@users.noreply.github.com>
2026-02-26 18:52:26 +0100

    fast fix

--- src/modules/SideBar/SideBar.js
+++ src/modules/SideBar/SideBar.js
@@ -2,12 +2,17 @@ import style from "./SideBar.module.css";
 import { useState } from "react";
 function SideBar({className}) {
 	
+	const [ShowPanel, setShowPanel] = useState(false);
+const changeState = () => {
+  setShowPanel(prev => !prev);
+};
   return (
+	<>
+<button onClick={changeState}>test</button>
+		<div className={`${className} ${ShowPanel  ? style.sideBarLocal : style.sideBarLocalDezactive }`} >
 
-		<div className={className}>
-			<p>test</p>
 		</div>
-  );
+  </>);
 }
 
 export default SideBar;
